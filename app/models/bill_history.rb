b52fe0ffe24519ea0294892af520531676d8c5d9
kiran <kiran@kiran-laptop.(none)>
2011-11-30 21:01:10 +0530

    fix bill generation issue

--- app/models/bill_history.rb
+++ app/models/bill_history.rb
@@ -1,6 +1,6 @@
 class BillHistory < ActiveRecord::Base
   belongs_to :paper
   belongs_to :employee
-  validates_uniqueness_of :month, :scope =>  [:year]
+  validates_uniqueness_of :month, :scope =>  [:year, :employee_id]
   validates_presence_of :month, :year, :employee_id
 end
