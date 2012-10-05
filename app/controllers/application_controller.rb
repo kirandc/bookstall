9fa19766338f30ef1d2b223eadee5b89d79b0dcc
Kiran Chaudhari <kiran@joshsoftware.com>
2012-10-05 22:50:03 +0530

    remove ip changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   USER, PASSWORD = 'rupesh', 'rupesh123'
 
- # before_filter :authentication_check
+  before_filter :authentication_check
   private
   def authentication_check
     authenticate_or_request_with_http_basic do |user, password|
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-  @ip = request.remote_ip
   end
 end
