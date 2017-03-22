615bc3e337390b6560334d781d2c9f0049bc1b63
Kiran Chaudhari <kiran@joshsoftware.com>
2017-03-22 16:30:40 +0530

    Updated print pages

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -9,7 +9,7 @@ class BillsController < ApplicationController
 
 
   def create
-    @bill_history = BillHistory.new(params[:bill_history])    
+    @bill_history = BillHistory.new(bill_history_params)    
     @bill_history.save!
     @customers = @bill_history.employee.customers
     @customers.each do | customer|
@@ -28,7 +28,7 @@ class BillsController < ApplicationController
     @bill_history = BillHistory.find(params[:id])
     if !@bill_history.blank?
       cus_id = @bill_history.employee.customers.collect(&:id)
-      @bills = Bill.find(:all, :conditions => ["month = ? and year = ? and customer_id IN(?)", @bill_history.month, @bill_history.year, cus_id])
+      @bills = Bill.where("month = ? and year = ? and customer_id IN(?)", @bill_history.month, @bill_history.year, cus_id)
       @bills.each do |bill|
         bill.destroy
       end if !@bills.blank?
@@ -44,7 +44,7 @@ class BillsController < ApplicationController
   def search
     @total_paid = Bill.sum(:amount, :conditions => ["paper_id = ? and month = ? and year = ? and is_paid = 1", params[:bill][:paper_id], params[:bill][:month], params[:bill][:year]])
     @total_unpaid = Bill.sum(:amount, :conditions => ["paper_id = ? and month = ? and year = ? and is_paid = 0", params[:bill][:paper_id], params[:bill][:month], params[:bill][:year]])
-    @bills = Bill.paginate(:all, :conditions => ["paper_id = ? and month = ? and year = ? and is_paid = ?", params[:bill][:paper_id], params[:bill][:month], params[:bill][:year], params[:bill][:is_paid]],:page => params[:page], :per_page => 1000)
+    @bills = Bill.where("paper_id = ? and month = ? and year = ? and is_paid = ?", params[:bill][:paper_id], params[:bill][:month], params[:bill][:year], params[:bill][:is_paid]).page(params[:page]).per_page(1000)
   end
 
   def bill_print
@@ -62,12 +62,17 @@ class BillsController < ApplicationController
     @reciept_type = params[:reciept_type] 
     @employee = Employee.find(params[:employee_id])
     if not params[:employee_id].blank?
-      @customers = Customer.find(:all, :conditions => ["employee_id = ?", params[:employee_id]])
+      @customers = Customer.where("employee_id = ?", params[:employee_id])
     else
-      @customers = Customer.find(:all, :order => "custid ASC")
+      @customers = Customer.order("custid ASC")
     end
     @month = params[:bill][:month]
     @year = params[:bill][:year]
     render :layout => false
+  end
+   
+  private
+  def bill_history_params
+    params.fetch(:bill_history, {}).permit(:month, :year, :employee_id)
   end
 end
