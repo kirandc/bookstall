90f0bb4e3aed175d9ae9ffa0793dfe1b7f0d00a9
kiran.c <kiran@joshsoftware.com>
2018-11-09 19:19:10 +0530

    fix security issue

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module TestApp
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
+    config.assets.compile = false
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
