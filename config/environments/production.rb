af0bf2a433f1db4bb4bed2b17a0d7515d15dd3e0
Kiran Chaudhari <kiran.chaudhari3423@gmail.com>
2021-01-18 19:28:40 +0530

    Added asset files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ TestApp::Application.configure do
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
-
+  config.log_level = :info
   # For nginx:
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 
